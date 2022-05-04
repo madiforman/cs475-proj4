8c0eb96b21487cf54dc80566447e3d0259067744
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-05-04 15:52:28 -0400

    trying to delete edges

--- adj_list.c
+++ adj_list.c
@@ -48,6 +48,66 @@ void rag_alloc(Graph *graph, int pid, int lockid)
     graph->list[lockid] = new;
 }
 
+void delete_edge(Graph *g, int src, int dst)
+{
+    for (int i = 0; i < g->size; i++)
+    {
+        node *n = g->list[i];
+        if (n->vertex == src)
+        {
+            node *temp = n->next;
+            printf("%d\n", n->vertex);
+            printf("%d\n,", n->next->vertex);
+            // while (temp != NULL)
+            // {
+
+            //     if (temp->vertex == dst)
+            //     {
+            //     }
+            //     temp = temp->next;
+            // }
+        }
+    }
+}
+void print_g(Graph *g)
+{
+
+    for (int i = 0; i < g->size; i++)
+    {
+        node *temp = g->list[i];
+        printf("\nVertex %d: ", i);
+        while (temp)
+        {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+node *find_prev(Graph *g, int v, int u)
+{
+    node *N;
+    for (int i = 0; i < g->size; i++)
+    {
+        if (g->list[i]->vertex == u)
+        {
+
+            N = g->list[i];
+            printf("here\n");
+            while (N->next != NULL)
+            {
+
+                if (N->next->vertex == v)
+                {
+                    return N;
+                }
+                N = N->next;
+            }
+            return NULL;
+        }
+    }
+    return NULL;
+}
 int cycle_util(Graph *g, int v, int *visited, int *recursive)
 {
     visited[v] = 1;
@@ -96,7 +156,13 @@ int main()
     rag_request(g, 1, 2);
     rag_request(g, 2, 3);
     rag_request(g, 3, 1);
-    int status = isCycle(g);
-    printf("%d\n", status);
+    print_g(g);
+    node *n = find_prev(g, 3, 1);
+    printf("%d\n", n->vertex);
+    // printf("**********\n");
+    // delete_edge(g, 3, 1);
+    // print_g(g);
+    //  int status = isCycle(g);
+    //  printf("%d\n", status);
     return 0;
 }
